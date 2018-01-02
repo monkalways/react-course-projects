1bd21880a6c84ec1fc5514447b576ce56146cfdd
Wei Wei <weiway@outlook.com>
2018-01-01 21:43:38 -0700

    Add es6 export

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,13 @@
-console.log('app.js is running!');
+// import './utils';
+// import { square, add } from './utils';
+
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+import { isAdult, canDrink } from './person';
+
+console.log(isAdult(20));
+console.log(isAdult(17));
+console.log(canDrink(20));
+console.log(canDrink(22));
