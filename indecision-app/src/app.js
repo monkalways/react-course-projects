99f2a1e8a1d1843c758dbe8b3f24bc82b1d3e614
WeiW_WK <WeiW_WK@police.edmonton.ab.ca>
2018-01-04 09:01:06 -0700

    Add window.React = React

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -5,4 +5,8 @@ import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+// Exposes the React library globally in the browser, 
+// which ensures all calls to React.createElement are assured to work.
+window.React = React;
+
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
