c74025b8d38e26dabb1106860c8bf9e2f08b3803
WeiW_WK <WeiW_WK@police.edmonton.ab.ca>
2018-01-05 11:10:05 -0700

    Separate components into separate files.

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -9,31 +9,4 @@ import './styles/styles.scss';
 // which ensures all calls to React.createElement are assured to work.
 window.React = React;
 
-const ExpenseDashboardPage = () => (
-    <div>Expense Dashboard Component</div>
-);
-
-const AddExpensePage = () => (
-    <div>Add Expense Component</div>
-);
-
-const EditExpensePage = () => (
-    <div>Edit Expense Component</div>
-);
-
-const HelpPage = () => (
-    <div>Help Component</div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
