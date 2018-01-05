8ac03ed65431885378655a8e013f8371e37bf353
WeiW_WK <WeiW_WK@police.edmonton.ab.ca>
2018-01-05 15:59:09 -0700

    Add action generators

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,16 +1,35 @@
 import { createStore } from 'redux';
 
+// Action generators
+
+const increment = ({incrementBy = 1} = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrement = ({decrementBy = 1} = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const reset = () => ({
+    type: 'RESET'
+});
+
+const set = ({count}) => ({
+    type: 'SET',
+    count
+});
+
 const store = createStore((state = { count: 0 }, action) => {
     switch(action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'RESET':
             return {
@@ -32,26 +51,11 @@ const unsubscribe = store.subscribe(() => {
 // Actions
 
 // increment, decrement, reset
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
-store.dispatch({
-    type: 'INCREMENT'
-});
-store.dispatch({
-    type: 'DECREMENT'
-});
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-});
-store.dispatch({
-    type: 'RESET'
-});
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
+store.dispatch(increment({incrementBy: 5}));
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement({decrementBy: 5}));
+store.dispatch(reset());
+store.dispatch(set({count: -101}));
 
 unsubscribe();
