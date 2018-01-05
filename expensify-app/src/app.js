fa5798cf07ec453ac621a3fb47f887b15aa7502d
WeiW_WK <WeiW_WK@police.edmonton.ab.ca>
2018-01-05 08:50:36 -0700

    Add react-router

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -8,4 +9,31 @@ import './styles/styles.scss';
 // which ensures all calls to React.createElement are assured to work.
 window.React = React;
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+    <div>Expense Dashboard Component</div>
+);
+
+const AddExpensePage = () => (
+    <div>Add Expense Component</div>
+);
+
+const EditExpensePage = () => (
+    <div>Edit Expense Component</div>
+);
+
+const HelpPage = () => (
+    <div>Help Component</div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
