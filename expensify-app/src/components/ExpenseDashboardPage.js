70369527c280592dbde365c51cf0b4df37c2648c
WeiW_WK <WeiW_WK@police.edmonton.ab.ca>
2018-01-19 14:18:46 -0700

    Add expense summary to dashboard

--- expensify-app/src/components/ExpenseDashboardPage.js
+++ expensify-app/src/components/ExpenseDashboardPage.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
 
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import getVisibleExpenses from '../selectors/expenses';
+import getExpensesTotal from '../selectors/expenses-total';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        <ExpenseListFilters />
-        <ExpenseList />
-    </div>
-);
+export const ExpenseDashboardPage = ({ expenses, expensesTotal }) => {
+    const count = expenses ? expenses.length : 0;
+    const expenseWording = count > 1 ? 'expenses' : 'expense';
+    const formattedTotal = numeral(expensesTotal/100).format('$0,0.00');
+    return (
+        <div>
+            <p>{`Viewing ${count} ${expenseWording} totalling ${formattedTotal}`}</p>
+            <ExpenseListFilters />
+            <ExpenseList />
+        </div>
+    )
+};
 
-export default ExpenseDashboardPage;
+const mapStateToProps = (state) => ({
+    expenses: getVisibleExpenses(state.expenses, state.filters),
+    expensesTotal: getExpensesTotal(getVisibleExpenses(state.expenses, state.filters))
+});
+
+export default connect(mapStateToProps)(ExpenseDashboardPage);
--- expensify-app/src/tests/components/ExpenseDashboardPage.test.js
+++ expensify-app/src/tests/components/ExpenseDashboardPage.test.js
@@ -2,9 +2,17 @@ import 'react-dates/initialize' // to make react-dates work
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import ExpenseDashboardPage from '../../components/ExpenseDashboardPage';
+import { ExpenseDashboardPage}  from '../../components/ExpenseDashboardPage';
+import expenses from '../fixtures/expenses';
 
-test('should render ExpenseDashboardPage', () => {
-    const wrapper = shallow(<ExpenseDashboardPage />);
+test('should render ExpenseDashboardPage with single expense', () => {
+    const expensesTotal = 195;
+    const wrapper = shallow(<ExpenseDashboardPage expenses={[expenses[0]]} expensesTotal={expensesTotal} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseDashboardPage with multiple expenses', () => {
+    const expensesTotal = 114195;
+    const wrapper = shallow(<ExpenseDashboardPage expenses={expenses} expensesTotal={expensesTotal} />);
     expect(wrapper).toMatchSnapshot();
 });
