8e3510fed24e0b0fccaac2b8e58610df6324b50a
WeiW_WK <WeiW_WK@police.edmonton.ab.ca>
2018-01-30 14:28:47 -0700

    Store user data in user area

--- expensify-app/src/actions/expenses.js
+++ expensify-app/src/actions/expenses.js
@@ -9,7 +9,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-    return (dispatch) => {
+    return (dispatch, getState) => {
+        const { uid } = getState().auth;
         const {
             description = '',
             note = '',
@@ -24,7 +25,7 @@ export const startAddExpense = (expenseData = {}) => {
             createdAt
         };
 
-        return database.ref('expenses').push(expense).then((ref) => {
+        return database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
             console.log('i am running')
             dispatch(addExpense({
                 id: ref.key,
@@ -41,8 +42,9 @@ export const removeExpense = ({ id } = {}) => ({
 });
 
 export const startRemoveExpense = ({ id } = {}) => {
-    return (dispatch) => {
-        return database.ref(`expenses/${id}`).remove().then(() => {
+    return (dispatch, getState) => {
+        const { uid } = getState().auth;
+        return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
             dispatch(removeExpense({id}));
         });
     };
@@ -57,8 +59,9 @@ export const editExpense = (id, updates) => ({
 
 // START_EDIT_EXPENSE
 export const startEditExpense = (id, updates) => {
-    return (dispatch) => {
-        return database.ref(`expenses/${id}`).update(updates).then(() => {
+    return (dispatch, getState) => {
+        const { uid } = getState().auth;
+        return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
             dispatch(editExpense(id, updates));
         });
     };
@@ -73,8 +76,9 @@ export const setExpenses = (expenses) => ({
 
 // START_SET_EXPENSES
 export const startSetExpenses = (expenses) => {
-    return (dispatch) => {
-        return database.ref('expenses').once('value').then( snapshot => {
+    return (dispatch, getState) => {
+        const { uid } = getState().auth;
+        return database.ref(`users/${uid}/expenses`).once('value').then( snapshot => {
             const expenses = [];
             snapshot.forEach(childSnapshot => {
                 expenses.push({
